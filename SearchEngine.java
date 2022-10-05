91ead54298a5ed0aea70d01067801c118337d4b3
rhoqan <114369881+rhoqan@users.noreply.github.com>
2022-10-05 15:20:58 -0700

    Create SearchEngine.java
    String Search Engine

---
+++ SearchEngine.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    List<String> strsList = new ArrayList<>();
+    List<String> strsList2 = new ArrayList<>();
+    String str = "";
+
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    strsList.add(parameters[1]);
+                }
+                return parameters[1];
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                str = "";
+                for (String s: strsList) {
+                    if (s.contains(parameters[1])) {
+                        str += s + " ";
+                    }
+                }
+                return str;
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }    
+}
